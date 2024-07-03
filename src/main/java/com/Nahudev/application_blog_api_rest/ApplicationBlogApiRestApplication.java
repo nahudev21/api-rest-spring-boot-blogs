abce33964def2eacd21da622fc856b2147076cc6
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-07-02 21:33:08 -0300

    Subi el proyecto base

---
+++ src/main/java/com/Nahudev/application_blog_api_rest/ApplicationBlogApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.Nahudev.application_blog_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBlogApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBlogApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Nahudev/application_blog_api_rest/ApplicationBlogApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nahudev.application_blog_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBlogApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
