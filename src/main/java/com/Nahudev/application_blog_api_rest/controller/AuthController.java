8c54047f12cdeb269823692f8dffae7e033ea400
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-07-13 21:06:29 -0300

    Registro de Usuarios

--- src/main/java/com/Nahudev/application_blog_api_rest/controller/AuthController.java
+++ src/main/java/com/Nahudev/application_blog_api_rest/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.Nahudev.application_blog_api_rest.controller;
 
 import com.Nahudev.application_blog_api_rest.dto.LoginDTO;
+import com.Nahudev.application_blog_api_rest.dto.RegisterDTO;
+import com.Nahudev.application_blog_api_rest.model.Rol;
+import com.Nahudev.application_blog_api_rest.model.UserEntity;
+import com.Nahudev.application_blog_api_rest.repository.IRolRepository;
+import com.Nahudev.application_blog_api_rest.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,6 +29,15 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Autowired
+    private IRolRepository rolRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDTO loginDTO) {
         Authentication authentication = authenticationManager.authenticate(
@@ -27,6 +45,27 @@ public class AuthController {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseEntity<>("Inicio de sesión con exito!", HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody RegisterDTO registerDTO) {
+
+        if (userRepository.existsByUsername(registerDTO.getUsername())) {
+            return new ResponseEntity<>("Ese nombre de usuario ya existe", HttpStatus.BAD_REQUEST);
+        } else if (userRepository.existsByEmail(registerDTO.getEmail())) {
+            return new ResponseEntity<>("Ya existe un usuario con ese email", HttpStatus.BAD_REQUEST);
+        }
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(registerDTO.getUsername());
+        userEntity.setEmail(registerDTO.getEmail());
+        userEntity.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+        Rol roles = rolRepository.findByName("ROLE_ADMIN").get();
+        userEntity.setRoles(Collections.singleton(roles));
+
+        userRepository.save(userEntity);
+        return new ResponseEntity<>("Usuario Registrado exitosamente!", HttpStatus.CREATED);
     }
 
 }
---
+++ src/main/java/com/Nahudev/application_blog_api_rest/dto/RegisterDTO.java
@@ -0,0 +1,22 @@
+package com.Nahudev.application_blog_api_rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterDTO {
+
+    private String name;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+}
